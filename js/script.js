8e42017b8a8a501258813dfdcef55fa1f84029d0
Luca <luca.lorenzetti0@gmail.com>
2021-04-07 15:18:48 +0200

    Inserita la lista delle mail

--- js/script.js
+++ js/script.js
@@ -1,7 +1,37 @@
+/* Descrizione:
+
+    Attraverso l'apposita API di Boolean
+    https://flynn.boolean.careers/exercises/api/random/mail
+    generare 10 indirizzi email e stamparli in pagina all'interno di una lista. 
+*/
+
 let app = new Vue({
     el: '#root',
-    date:{
+ 
+    data:{
+        emailsList: []
+    },
 
+    mounted(){
+        this.emailsList = this.getEmails(10);
+    },
+
+    methods:{
+        // Funzione per richiedere le mail
+        getEmails: function(num){
+
+            const email = [];
+
+            for (let i = 0; i < num; i++) {
+
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then(function (response) {
+                 email.push(response.data.response);
+               });
+            }
+
+           return email;
+        }
     }
 });
 
